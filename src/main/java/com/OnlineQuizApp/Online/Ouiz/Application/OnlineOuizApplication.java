a4dbc9e547a65b6c80820f77d93c3b608f9b9bf4
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-01 11:41:22 +0545

    entity user

---
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/OnlineOuizApplication.java
@@ -0,0 +1,13 @@
+package com.OnlineQuizApp.Online.Ouiz.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOuizApplication.class, args);
+	}
+
+}
