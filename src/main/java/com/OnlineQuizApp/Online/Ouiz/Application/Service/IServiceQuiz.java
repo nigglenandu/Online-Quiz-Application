39ea30c8e4d50fa011d86e6f51197ae8bd451319
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-03 14:30:10 +0545

    quiz service added

--- src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Service/IServiceQuiz.java
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Service/IServiceQuiz.java
@@ -1,6 +1,13 @@
 package com.OnlineQuizApp.Online.Ouiz.Application.Service;
 
+import com.OnlineQuizApp.Online.Ouiz.Application.Entity.Quiz;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IServiceQuiz {
-
-
+    List<Quiz> getAllQuiz();
+    Optional<Quiz> getQuizById(Long id);
+    void addQuiz(Quiz quiz);
+    boolean deleteById(Long id);
 }
--- src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Service/QuizServiceImpl.java
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Service/QuizServiceImpl.java
@@ -1,12 +1,40 @@
 package com.OnlineQuizApp.Online.Ouiz.Application.Service;
 
+import com.OnlineQuizApp.Online.Ouiz.Application.Entity.Quiz;
 import com.OnlineQuizApp.Online.Ouiz.Application.Repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizServiceImpl implements IServiceQuiz{
     @Autowired
     private QuizRepository quizRepository;
 
+    @Override
+    public List<Quiz> getAllQuiz() {
+        return quizRepository.findAll();
+    }
+
+    @Override
+    public Optional<Quiz> getQuizById(Long id) {
+        return quizRepository.findById(id);
+    }
+
+    @Override
+    public void addQuiz(Quiz quiz) {
+        quizRepository.save(quiz);
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        if(quizRepository.existsById(id)){
+            quizRepository.deleteById(id);
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
