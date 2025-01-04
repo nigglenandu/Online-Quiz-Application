3b266dfe0b35bb9a3cf32712c2f508c4b19dd260
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-04 18:29:41 +0545

    quiz controller

--- src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuizController.java
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuizController.java
@@ -1,4 +1,48 @@
 package com.OnlineQuizApp.Online.Ouiz.Application.Controller;
 
+import com.OnlineQuizApp.Online.Ouiz.Application.Entity.Quiz;
+import com.OnlineQuizApp.Online.Ouiz.Application.Service.IServiceQuiz;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quizzes")
 public class QuizController {
+
+    public final IServiceQuiz serviceQuiz;
+
+    public QuizController(IServiceQuiz serviceQuiz) {
+        this.serviceQuiz = serviceQuiz;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Quiz>> getAllQuizzes(){
+        return new ResponseEntity<>(serviceQuiz.getAllQuiz(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Quiz> getQuizById(@PathVariable Long id){
+        return serviceQuiz.getQuizById(id)
+                .map(quiz -> new ResponseEntity<>(quiz, HttpStatus.OK) )
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Void> createQuiz(@RequestBody Quiz quiz){
+        serviceQuiz.addQuiz(quiz);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        boolean isDeleted = serviceQuiz.deleteById(id);
+        if(isDeleted){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
