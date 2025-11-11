c022664a7deb41542a621f22930a2b446893b5bc
[Niggle Nandu] <nigglenandu@gmail.com>
2025-11-11 15:32:07 +0545

    fine

--- src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuestionController.java
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuestionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/questionsssssss")
+@RequestMapping("/question")
 public class QuestionController {
 
     private final IServiceQuestion serviceQuestion;
