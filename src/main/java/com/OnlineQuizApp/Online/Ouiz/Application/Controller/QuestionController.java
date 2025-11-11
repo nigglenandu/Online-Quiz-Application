b475df0a5b1ab40467a3f45d7ff5c6645fd9b51f
[Niggle Nandu] <nigglenandu@gmail.com>
2025-11-11 15:38:32 +0545

    Revert "admin"
    This reverts commit ab855bc0ac30dd4054e75363474b1dc7fdab1762.

--- src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuestionController.java
+++ src/main/java/com/OnlineQuizApp/Online/Ouiz/Application/Controller/QuestionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/questionsssssss")
 public class QuestionController {
 
     private final IServiceQuestion serviceQuestion;
